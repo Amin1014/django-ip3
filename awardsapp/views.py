75ea6cfc7b86a2fc35f3e6b3d00892b6f1f3ab2d
Amin1014 <aminabdi570@gmail.com>
2021-09-18 12:14:36 +0300

    adding views

--- awardsapp/views.py
+++ awardsapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from __future__ import unicode_literals
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Image,Location,tags, Profile, Review, NewsLetterRecipients, Like, Project
+from django.http  import HttpResponse, Http404, HttpResponseRedirect, JsonResponse
+from django.contrib.auth.decorators import login_required
+# from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+from .forms import NewImageForm, UpdatebioForm, ReviewForm, NewProjectForm
+from .email import send_welcome_email
+from .forms import NewsLetterForm
+
+
+
